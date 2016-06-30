f536db3e0067c88f365bb08e0cce4ac0067e9945
Guido Navalesi <gnavalesi@baufest.com>
2016-06-30 12:22:39 -0300

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha'),
+    istanbul = require('gulp-istanbul'),
+    coveralls = require('gulp-coveralls');
+
+gulp.task('test', function() {
+	return gulp.src('./test/**/*.js')
+	           .pipe(mocha());
+});
+
+gulp.task('pre-coverage', function () {
+	return gulp.src(['./src/**/*.js'])
+		.pipe(istanbul())
+		.pipe(istanbul.hookRequire());
+});
+
+gulp.task('coverage', ['pre-coverage'], function () {
+	return gulp.src('./test/**/*.js')
+		.pipe(mocha())
+		.pipe(istanbul.writeReports())
+		.pipe(istanbul.enforceThresholds({thresholds: {global: 70}}));
+});
+
+gulp.task('ci', ['coverage'], function () {
+	return gulp.src('./coverage/**/lcov.info')
+		.pipe(coveralls());
+});
